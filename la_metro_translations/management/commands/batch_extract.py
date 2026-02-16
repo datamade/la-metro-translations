10994b37fbacd283b90ad8353895a3e2f1c37a06
Xavier Medrano <xaviermedrano@Xaviers-MacBook-Pro.local>
2026-02-16 12:21:25 -0500

    add mgmt command to extract text from all documents that need content objs

---
+++ la_metro_translations/management/commands/batch_extract.py
@@ -0,0 +1,72 @@
+import logging
+
+from django.core.management.base import BaseCommand
+
+from la_metro_translations.models import (
+    Document,
+    DocumentContent,
+    DocumentTranslation,
+    TranslationFile,
+)
+from la_metro_translations.services import MistralOCRService
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    """
+    Extract text from documents that still need content, and create related objects."
+    """
+
+    help = (
+        "Perform OCR text extraction on all Documents that do not yet have "
+        "a related DocumentContent object, then create an"
+        "English DocumentTranslation and TranslationFiles."
+    )
+
+    def handle(self, *args, **kwargs):
+        documents = Document.objects.filter(content__isnull=True)
+        if len(documents) == 0:
+            logger.info("All Documents currently have content! Not performing OCR.")
+            return
+        else:
+            logger.info(f"Performing OCR on {len(documents)} Document(s)...")
+
+        extractions = MistralOCRService.batch_extract(documents)
+        contents_created = 0
+        contents_skipped = 0
+
+        for result in extractions:
+            try:
+                document = Document.objects.get(
+                    document_type=result["document_type"],
+                    document_id=result["document_id"],
+                )
+            except Document.DoesNotExist:
+                logger.error(
+                    "Document.DoesNotExist: Could not find a matching Document with "
+                    f"document_type={result['document_type']} and "
+                    f"document_id={result['document_id']}; Skipping..."
+                )
+                contents_skipped += 1
+                continue
+
+            document_content = DocumentContent.objects.create(
+                markdown=result["markdown"], document=document
+            )
+            english_translation = DocumentTranslation.objects.create(
+                markdown=document_content.markdown,
+                language="english",
+                document_content=document_content,
+            )
+            TranslationFile.objects.create(
+                format="pdf",
+                url=document.source_url,
+                document_translation=english_translation,
+            )
+            contents_created += 1
+            # TODO: create rtf and md versions of the file, upload to s3, store urls
+
+        logger.info(f"DocumentContents w/related objects created: {contents_created}")
+        logger.info(f"DocumentContents skipped: {contents_skipped}")
+        logger.info("--- Finished! ---")
