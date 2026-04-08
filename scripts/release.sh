a234c814f3cbc696d5af24f0a3dd895b45629584
Xavier Medrano <xaviermedrano@Xaviers-MacBook-Pro.local>
2026-04-08 12:20:41 -0400

    create initial docs by default on review apps

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,8 +7,7 @@ python manage.py migrate --noinput
 python manage.py createcachetable && python manage.py clear_cache
 
 # Optional: Check if initial data exists, and if not, run initial imports.
-# To enable, uncomment this block and change example_data to match a table
-# name in your app.
-# if [ `psql ${DATABASE_URL} -tAX -c "SELECT COUNT(*) FROM example_table"` -eq "0" ]; then
-#    # Define an initial data loading command here, if one exists.
-# fi
+if [ `psql ${DATABASE_URL} -tAX -c "SELECT COUNT(*) FROM la_metro_translations_document"` -eq "0" ]; then
+   # Define an initial data loading command here, if one exists.
+   python manage.py create_initial_docs
+fi
