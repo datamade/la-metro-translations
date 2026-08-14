4fd725539819678f8fc1eba1380a27f351cc83df
Lily Ye <lily.ye@datamade.us>
2026-08-14 13:06:15 -0700

    Shorten lines for linter

--- tests/test_subject_normalization.py
+++ tests/test_subject_normalization.py
@@ -1,7 +1,8 @@
 import pytest
 from la_metro_translations.services.ocr import normalize_subject_lines
 
-# Note that all test data should be written flush left so that we avoid problems with leading whitespace (which the Mistral OCR data does not have).
+# Note that all test data should be written flush left so that we avoid problems
+# with leading whitespace (which the Mistral OCR data does not have).
 
 
 @pytest.mark.parametrize(
@@ -30,35 +31,35 @@ SUBJECT: 1000 (MINUTES)
         (
             """
 3. SUBJECT: REMARKS BY THE CHAIR [2025-0834](https://metro.legistar.com/gateway.aspx?m=l&id=/matter.aspx?key=11853)
-44. SUBJECT: PUBLIC HEARING ON RESOLUTIONS OF NECESSITY FOR EAST SAN FERNANDO VALLEY LIGHT RAIL TRANSIT PROJECT [2025-0596](https://metro.legistar.com/gateway.aspx?m=l&id=/matter.aspx?key=11615)
+44. SUBJECT: PUBLIC HEARING ON RESOLUTIONS OF NECESSITY [2025-0596](https://metro.legistar.com?m=l&id=/123)
         """,
             """
 **3. SUBJECT: REMARKS BY THE CHAIR [2025-0834](https://metro.legistar.com/gateway.aspx?m=l&id=/matter.aspx?key=11853)**
-**44. SUBJECT: PUBLIC HEARING ON RESOLUTIONS OF NECESSITY FOR EAST SAN FERNANDO VALLEY LIGHT RAIL TRANSIT PROJECT [2025-0596](https://metro.legistar.com/gateway.aspx?m=l&id=/matter.aspx?key=11615)**
+**44. SUBJECT: PUBLIC HEARING ON RESOLUTIONS OF NECESSITY [2025-0596](https://metro.legistar.com?m=l&id=/123)**
         """,
         ),
         (
             """
 ### SUBJECT: GENERAL PUBLIC COMMENT
 ### 3. SUBJECT: CONSTRUCTION ZONE ENHANCED ENFORCEMENT PROGRAM (COZEEP) SERVICES
-### 14. SUBJECT: REPORT BY THE CHIEF EXECUTIVE OFFICER [2025-0835](https://metro.legistar.com/gateway.aspx?m=l&id=/matter.aspx?key=11854)
+### 14. SUBJECT: REPORT BY THE CHIEF EXECUTIVE OFFICER [2025-0835](https://metrom/gway.aspx?m=l&id=/mar.aspx?key=11615)
         """,
             """
 **SUBJECT: GENERAL PUBLIC COMMENT**
 **3. SUBJECT: CONSTRUCTION ZONE ENHANCED ENFORCEMENT PROGRAM (COZEEP) SERVICES**
-**14. SUBJECT: REPORT BY THE CHIEF EXECUTIVE OFFICER [2025-0835](https://metro.legistar.com/gateway.aspx?m=l&id=/matter.aspx?key=11854)**
+**14. SUBJECT: REPORT BY THE CHIEF EXECUTIVE OFFICER [2025-0835](https://metrom/gway.aspx?m=l&id=/mar.aspx?key=11615)**
         """,
         ),
         (
             """
 **SUBJECT: GENERAL PUBLIC COMMENT**
 **5. SUBJECT: FEDERAL TRANSIT ADMINISTRATION SECTION 5310 PROGRAM**
-**4. SUBJECT: REPORT BY THE CHIEF EXECUTIVE OFFICER [2025-0835](https://metro.legistar.com/gateway.aspx?m=l&id=/matter.aspx?key=11854)**
+**4. SUBJECT: REPORT BY THE CHIEF EXECUTIVE OFFICER [2025-0835](https://metrom/gway.aspx?m=l&id=/mar.aspx?key=11615)**
         """,
             """
 **SUBJECT: GENERAL PUBLIC COMMENT**
 **5. SUBJECT: FEDERAL TRANSIT ADMINISTRATION SECTION 5310 PROGRAM**
-**4. SUBJECT: REPORT BY THE CHIEF EXECUTIVE OFFICER [2025-0835](https://metro.legistar.com/gateway.aspx?m=l&id=/matter.aspx?key=11854)**
+**4. SUBJECT: REPORT BY THE CHIEF EXECUTIVE OFFICER [2025-0835](https://metrom/gway.aspx?m=l&id=/mar.aspx?key=11615)**
         """,
         ),
     ],
